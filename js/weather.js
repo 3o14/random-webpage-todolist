70ee4a570edb97fe16a8fdf5e16d5b0b0504010d
3o14 <dldnjswn4265@gmail.com>
2023-01-09 01:14:55 +0900

    날씨 기능 구현완료

--- js/weather.js
+++ js/weather.js
@@ -1,6 +1,6 @@
 const weather = document.querySelector("#weather span:first-child");
 const city = document.querySelector("#weather span:last-child");
-const API_KEY = "241051bf13976dd3ddf8b8d9f247255e";
+const API_KEY = "43c2731e9d9ab368b311af53292590ac";
 
 function onGeoOK(position){
     const lat = position.coords.latitude;
