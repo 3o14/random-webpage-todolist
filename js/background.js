96861c970952eaba7f0c67db54f88373b93ddee8
3o14 <dldnjswn4265@gmail.com>
2023-01-09 00:54:11 +0900

    날씨 기능 수정중&css

--- js/background.js
+++ js/background.js
@@ -3,8 +3,11 @@ const images = ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");
+const body = document.body;
 
-bgImage.src = `img/${chosenImage}`;
+body.style.backgroundImage = `url('img/${chosenImage}')`;
+body.style.backgroundSize = '100%';
+
 
 console.log(bgImage);
-document.body.appendChild(bgImage);
+// document.body.appendChild(bgImage);
--- js/weather.js
+++ js/weather.js
@@ -1,11 +1,17 @@
-const API_KEY = "43c2731e9d9ab368b311af53292590ac";
+const weather = document.querySelector("#weather span:first-child");
+const city = document.querySelector("#weather span:last-child");
+const API_KEY = "241051bf13976dd3ddf8b8d9f247255e";
 
 function onGeoOK(position){
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
-    fetch(url);
-    console.log(url);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
 }
 
 function onGeoerror(){
